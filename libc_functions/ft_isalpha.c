9a252b170fd4d176eb0756c39d8a9b8dac5f40cc
Sara <saruna@Maccabooka.local>
2021-08-27 12:34:12 +0930

    Libc functions

--- libc_functions/ft_isalpha.c
+++ libc_functions/ft_isalpha.c
@@ -1,9 +1,4 @@
 int	ft_isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	return ((c >='A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
