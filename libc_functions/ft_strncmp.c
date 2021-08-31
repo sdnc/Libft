6517e466533bbf263201de903d3b29d23d8a3e20
sdnc <dza.sara@gmail.com>
2021-08-31 11:39:15 +0930

    libc functions

--- libc_functions/ft_strncmp.c
+++ libc_functions/ft_strncmp.c
@@ -1,9 +1,7 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	i;
-	
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] && s2[i] && n--)
-		i++;
-	return (s1[i] - s2[i]);
+	while (s1[i] && s2[i] && n--)
+		if (*s1++ != *s2++)
+			return ((unsigned char *)*--s1 - (unsigned char *)*--s2);
+	return (0);
 }
