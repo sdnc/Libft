bbf31a0edbbc9a45a1db6eeacd37d6225b7c6f3f
sdnc <dza.sara@gmail.com>
2021-09-08 11:42:42 +0930

    libc functions 080921

--- libc_functions/ft_itoa.c
+++ libc_functions/ft_itoa.c
@@ -1,4 +1,28 @@
 char	*ft_itoa(int n)
 {
+	char		base[] = "0123456789";
+	size_t		len;
+	unsigned int	nbr;
+	char		*result;
 
+	if (n < 0)
+		nbr = -n;
+		len = 2;
+	else
+		nbr = n;
+		len = 1;
+	while ((nbr /= 10) > 0)
+		len++;
+	result = (char *)malloc((len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	result[len--] = '\0';
+	while ((nbr /= 10) >= 0)
+		result[len--] = base[nbr % 10];
+	if (n < 0)
+		result[0] = "-";
+	return (result);
 }
+
+// Can I save one line of code by (nbr /= 10) >= 0? 
+
